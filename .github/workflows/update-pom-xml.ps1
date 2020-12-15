71054af10439caf8861c4f57e5a81aabee3a1e90
Kazumi OHIRA <dzeyelid@gmail.com>
2020-12-15 09:08:17 +0000

    Fix script

--- .github/workflows/update-pom-xml.ps1
+++ .github/workflows/update-pom-xml.ps1
@@ -2,13 +2,13 @@ $path = "sdk/pom.xml"
 [Reflection.Assembly]::LoadWithPartialName("System.Xml.Linq")
 $doc = [System.Xml.Linq.XElement]::Load($path)
 $ns = $doc.GetDefaultnamespace()
-$configrationElement = [System.Xml.Linq.XElement]::New(
-    $ns + "configration",
+$configurationElement = [System.Xml.Linq.XElement]::New(
+    $ns + "configuration",
     [System.Xml.Linq.XElement]::New(
     $ns + "gpgArguments",
     [System.Xml.Linq.XElement]::New($ns + "arg", "--pinentry-mode"),
     [System.Xml.Linq.XElement]::New($ns + "arg", "loopback")
     )
 )
-$doc.Descendants($ns + "plugin").Where({$_.Element($ns + "artifactId").Value -eq "maven-gpg-plugin"}).Descendants($ns + "execution").Add($configrationElement)
+$doc.Descendants($ns + "plugin").Where({$_.Element($ns + "artifactId").Value -eq "maven-gpg-plugin"}).Descendants($ns + "execution").Add($configurationElement)
 $doc.save($path)
